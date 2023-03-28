93b70523caee6a6d46fa04bc4f9385b29ad253e1
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-28 11:17:50 +0530

    using ejs layouts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let items = ['Buy Food',
     'Cook Food',
     'Eat Food'];
 
+let workItems = [];
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
 
@@ -22,13 +23,34 @@ app.get('/' , function(req , res){
     
     const day = today.toLocaleDateString('en-in', options);
    
-    res.render('list',{ kindOfDay : day , newListItem: items});
+    res.render('list',{ listTitle : day , newListItem: items});
 })
 
 app.post('/', function(req, res){
     const item = req.body.newItem;
-     items.push(item);
-    res.redirect('/');
+   
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect('/work')
+    }else{
+        items.push(item);
+        res.redirect('/');
+    }
+   
+})
+
+app.get('/work', function(req ,res ) { 
+    res.render('list', { listTitle : "Work List", newListItem : workItems});
+})
+
+app.post('/work', function() { 
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/work');
+})
+
+app.get('/about' , function(req, res) {
+    res.render('about')
 })
 
 app.listen(3000, function(){
