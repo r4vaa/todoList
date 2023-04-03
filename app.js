1ce1a064ef932291d802c081d5b1c4d0c7e7140c
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-03 19:22:42 +0530

    changes

--- app.js
+++ app.js
@@ -2,25 +2,65 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js')
+const mongoose = require('mongoose');
 
 
-const items = ['Buy Food',
-    'Cook Food',
-    'Eat Food'];
 
-const workItems = [];
+
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
-app.get('/' , function(req , res){
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', { useNewUrlParser : true});
 
-    const day = date.getDate()
+const itemsSchema = new mongoose.Schema ({
+    name : String
+});
 
-    res.render('list',{ listTitle : day , newListItem: items});
+const Item = mongoose.model('Item',itemsSchema);
+
+const item1 = new Item ({
+
+    name : 'welcome for todolist'
 })
+
+const item2 = new Item({
+    name : "Hit + button to add a new Item"
+})
+
+const item3 = new Item({
+    name : "<-- Hit this to delete an item"
+})
+
+const item5 = new Item({
+    name : 'Wait for lunch'
+})
+
+// item5.save()
+// .then(data => console.log(data + "Successfully addded   "));
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems)
+// .then(function(err) {
+//     if(err){
+//         console.log(err)
+//         console.log("successfully added")
+//     }
+// })
+
+
+app.get('/' , async function(req , res){
+
+    const items = await Item.find()
+
+    res.render('list',{ listTitle :"Today" , newListItems: items});
+})
+
 
 app.post('/', function(req, res){
     const item = req.body.newItem;
