c8617cce18faecf2aca98c662f8a6b3d81bc036e
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-04 00:19:07 +0530

    dynamic id lists

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js')
 const mongoose = require('mongoose');
+const { redirect } = require('express/lib/response');
 
 
 
@@ -36,54 +37,79 @@ const item3 = new Item({
     name : "<-- Hit this to delete an item"
 })
 
-const item5 = new Item({
+const item4 = new Item({
     name : 'Wait for lunch'
 })
 
 // item5.save()
 // .then(data => console.log(data + "Successfully addded   "));
 
-const defaultItems = [item1, item2, item3];
+const defaultItems = [item1, item2, item3, item4];
 
-// Item.insertMany(defaultItems)
-// .then(function(err) {
-//     if(err){
-//         console.log(err)
-//         console.log("successfully added")
-//     }
-// })
+const listSchema = { 
+    name : String,
+    items : [ itemsSchema ]
+}
+
+const List = mongoose.model('List', listSchema);
 
 
-app.get('/' , async function(req , res){
+app.get('/' , function(req , res){
 
-    const items = await Item.find()
-
-    res.render('list',{ listTitle :"Today" , newListItems: items});
+    const data =  Item.find({}).then(data => {
+        if(data.length === 0 ){
+            Item.insertMany(defaultItems)
+            .then(function(err) {
+                if(err){
+                    console.log(err)
+                    console.log("successfully added")
+                }
+            })
+            res.redirect('/');
+        }else{
+            res.render('list',{ listTitle :"Today" , newListItems: data});
+        }
+       
+    })
+    
 })
 
 
 app.post('/', function(req, res){
-    const item = req.body.newItem;
-   
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect('/work')
-    }else{
-        items.push(item);
-        res.redirect('/');
-    }
+    const itemName = req.body.newItem ;
+    const item = new Item({
+        name : itemName
+    });
+    item.save()
+    res.redirect('/')
    
 })
 
-app.get('/work', function(req ,res ) { 
-    res.render('list', { listTitle : "Work List", newListItem : workItems});
+app.post('/delete', async function(req, res) {
+    const checkedItemId = req.body.checkbox;
+    const deletedItem = await Item.findByIdAndRemove({ _id: checkedItemId })
+    res.redirect('/')
 })
 
-app.post('/work', function() { 
-    const item = req.body.newItem;
-    workItems.push(item);
-    res.redirect('/work');
-})
+app.get('/:list', async function(req, res) {
+   const customListName =  req.params.list;
+    List.findOne({name : customListName}).then(data => {
+        if(!data){
+            //Create a new list
+            const list = new List({
+                name : customListName,
+                items: defaultItems
+            })
+            list.save();
+            res.redirect('/' + customListName)
+        }else{
+            //Show an existing list
+            res.render('list',{ listTitle : data.name , newListItems: data.items})
+}})
+       
+} )
+
+
 
 app.get('/about' , function(req, res) {
     res.render('about')
